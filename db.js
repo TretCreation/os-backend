c5a5d0741dcdc9ddf67a11af3c14f56cf25813fc
stretyakov <stretyakov@graphpad.com>
2022-06-10 15:19:48 +0300

    [tret]

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(process.env.DATABASE_URL, {
+const dialectOptions =
+	process.env.MODE === "local"
+		? {}
+		: {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false,
+				},
+		  };
+
+module.exports = new Sequelize(process.env.DB_DBATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+	host: process.env.DB_HOSTNAME,
+	port: process.env.DB_PORT,
 	dialect: "postgres",
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false,
-		},
-	},
+	dialectOptions,
 });
