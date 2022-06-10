e8f1805f74a518a304366652ac330105788971f1
stretyakov <stretyakov@graphpad.com>
2022-06-10 14:54:06 +0300

    [tret] deploy

--- db.js
+++ db.js
@@ -2,4 +2,10 @@ const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize(process.env.DATABASE_URL, {
 	dialect: "postgres",
+	dialectOptions: {
+		ssl: {
+			require: true,
+			rejectUnauthorized: false,
+		},
+	},
 });
