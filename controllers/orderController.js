07223db5b721e1ca9eae3b60bdc96c0f8237eafa
stretyakov <stretyakov@graphpad.com>
2022-06-11 12:17:10 +0300

    [tret]

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -29,47 +29,6 @@ class OrderController {
 
 	async update(req, res, next) {
 		try {
-			const { id } = req.params;
-			let { name, price, brandId, typeId, info } = req.body;
-
-			const img = req.files?.img;
-			let fileName = "";
-			if (img) {
-				fileName = uuid.v4() + ".jpg";
-				img.mv(path.resolve(__dirname, "..", "static", fileName));
-				// TODO: remove old images
-			}
-
-			const result = await Product.update(
-				{ name, price, brandId, typeId, info, ...(img && { img: fileName }) },
-				{
-					where: { id },
-				}
-			);
-
-			if (info) {
-				info = JSON.parse(info);
-				info.forEach((i) => {
-					if (i.id.toString().slice(0, 3) === "id_") {
-						return ProductInfo.create({
-							title: i.title,
-							description: i.description,
-							productId: id,
-						});
-					}
-					return ProductInfo.update(
-						{
-							title: i.title,
-							description: i.description,
-						},
-						{
-							where: { id: i.id },
-						}
-					);
-				});
-			}
-
-			return res.json(result);
 		} catch (error) {
 			next(ApiError.internal(error.message));
 		}
